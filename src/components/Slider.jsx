89f79680c1865f7d82ef4b003a8a3a738474dfb4
darkelf2402 <93836889+darkelf2402@users.noreply.github.com>
2023-12-04 22:37:01 +0530

    New

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -14,7 +14,7 @@ const Carousel = () => {
       <div
         className={`h-[200px] xs:h-[400px] md:h-[710px]    rounded-[5px] flex-1   transition duration-300 relative ${
           selected === 0 ? 'bg-white transform scale-105' : 'bg-white'
-        } ${selected === 0 ? 'z-10 border border-gray-200' : 'z-0'}`}
+        } ${selected === 0 ? 'md:z-10 z-0 border border-gray-200' : 'z-0'}`}
         onClick={() => handleSelect(0)}
       >
         <Resume/>
@@ -22,7 +22,7 @@ const Carousel = () => {
       <div
         className={`flex-1  m-2 p-4 cursor-pointer transition md:h-[710px] duration-300 relative   rounded-[5px] ${
           selected === 1 ? ' transform scale-105 bg-white' : 'bg-white'
-        } ${selected === 1 ? 'z-10 border border-gray-200 ' : 'z-0'}`}
+        } ${selected === 1 ? 'md:z-10 z-0 border border-gray-200 ' : 'z-0'}`}
         onClick={() => handleSelect(1)}
       >
         <AboutMe/>
